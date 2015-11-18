8b365dc7c76bad490a61c4af89202721f582adb6
Arjo Bruijnes <arjo@weirdwater.net>
2015-11-18 13:58:32 +0100

    Contain code in an IIFE

--- twitch-on-air.js
+++ twitch-on-air.js
@@ -1,89 +1,92 @@
-window.onload = init;
+( function() {
+	var ON  = 1;
+	var OFF = 0;
+	var SIGN = $('#sign');
 
-var ON  = 1;
-var OFF = 0;
-var SIGN = $('#sign');
+	var streamUpdateInterval;
 
-var streamUpdateInterval;
+	init();
 
-function init() {
-	requestChannel('cmgtplays');
+	function init() {
+		requestChannel('cmgtplays');
 
-	// Event listeners
-	$("#search-btn").click(searchHandler);
-}
-
-function searchHandler() {
-	clearInterval(streamUpdateInterval);
-
-	var channelName = $('#search-inp').val();
-	requestChannel(channelName);
-}
-
-
-// CHANNEL DATA
-function requestChannel(channelName) {
-	$.when($.getJSON('https://api.twitch.tv/kraken/channels/' + channelName))
-		.then(updateChannelInfo, requestChannelError);
-}
-
-function updateChannelInfo(data) {
-	$('#channel').text(data.display_name);
-	$('#views').text(data.views);
-	$('#followers').text(data.followers);
-	requestStream();
-	streamUpdateInterval = setInterval(requestStream, 2000);
-}
-
-function requestChannelError(data) {
-	var error = data.responseJSON;
-	switch (error.status) {
-		case 404:
-			// Handle non existing channelname
-			alert('Channel does not exist.');
-			break;
-		default:
-			console.error(error.status + ' ' + error.error + ': ' + error.message);
+		// Event listeners
+		$("#search-btn").click(searchHandler);
 	}
-}
 
+	function searchHandler() {
+		clearInterval(streamUpdateInterval);
 
-// STREAM DATA
-function requestStream() {
-	var channelName = $('#channel').text();
-	console.log(channelName);
-	$.when($.getJSON('https://api.twitch.tv/kraken/streams/' + channelName))
-		.then(updateStreamInfo, requestChannelError);
-}
-
-function updateStreamInfo(data) {
-	if(data.stream) {
-		powerSign(ON);
-		$('#viewers').text(data.stream.viewers);
+		var channelName = $('#search-inp').val();
+		requestChannel(channelName);
 	}
-	else {
-		powerSign(OFF);
-		$('#viewers').text('0');
-	}
-}
 
-/**
- * Acts like a toggle if no paramaters are given.
- */
-function powerSign(state) {
-	switch (state) {
-		case 0:
-			SIGN.attr('data-on-air', false);
-			break;
-		case 1:
-			SIGN.attr('data-on-air', true);
-			break;
-		default:
-			if (SIGN.attr('data-on-air') == 'true') {
+
+	// CHANNEL DATA
+	function requestChannel(channelName) {
+		$.when($.getJSON('https://api.twitch.tv/kraken/channels/' + channelName))
+			.then(updateChannelInfo, requestChannelError);
+	}
+
+	function updateChannelInfo(data) {
+		$('#channel').text(data.display_name);
+		$('#views').text(data.views);
+		$('#followers').text(data.followers);
+		requestStream();
+		streamUpdateInterval = setInterval(requestStream, 2000);
+	}
+
+	function requestChannelError(data) {
+		var error = data.responseJSON;
+		switch (error.status) {
+			case 404:
+				// Handle non existing channelname
+				alert('Channel does not exist.');
+				break;
+			default:
+				console.error(error.status + ' ' + error.error + ': ' + error.message);
+		}
+	}
+
+
+	// STREAM DATA
+	function requestStream() {
+		var channelName = $('#channel').text();
+		console.log(channelName);
+		$.when($.getJSON('https://api.twitch.tv/kraken/streams/' + channelName))
+			.then(updateStreamInfo, requestChannelError);
+	}
+
+	function updateStreamInfo(data) {
+		if(data.stream) {
+			powerSign(ON);
+			$('#viewers').text(data.stream.viewers);
+		}
+		else {
+			powerSign(OFF);
+			$('#viewers').text('0');
+		}
+	}
+
+	/**
+	 * Controlls the "On Air sign".
+	 * Acts like a toggle if no paramaters are given.
+	 */
+	function powerSign(state) {
+		switch (state) {
+			case 0:
 				SIGN.attr('data-on-air', false);
-			}
-			else {
+				break;
+			case 1:
 				SIGN.attr('data-on-air', true);
-			}
+				break;
+			default:
+				if (SIGN.attr('data-on-air') == 'true') {
+					SIGN.attr('data-on-air', false);
+				}
+				else {
+					SIGN.attr('data-on-air', true);
+				}
+		}
 	}
-}
+})();
